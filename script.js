67cd016690e868498e451e1abe9d1fe409a4f2ad
Henrique Osiro <henriqueosiro@hotmail.com>
2019-11-12 20:53:03 -0300

    Criar a função secundária para validar se já houve vitória ou empate

--- script.js
+++ script.js
@@ -11,6 +11,24 @@ function play(event) {
             event.target.textContent = "O"
         }
         click++;
+        check();
+    }
+}
+
+function check() {
+    if ((box[0].innerText != "" && box[0].innerText == box[1].innerText && box[1].innerText == box[2].innerText) ||
+        (box[3].innerText != "" && box[3].innerText == box[4].innerText && box[4].innerText == box[5].innerText) ||
+        (box[6].innerText != "" && box[6].innerText == box[7].innerText && box[7].innerText == box[8].innerText) ||
+        (box[0].innerText != "" && box[0].innerText == box[3].innerText && box[3].innerText == box[6].innerText) ||
+        (box[1].innerText != "" && box[1].innerText == box[4].innerText && box[4].innerText == box[7].innerText) ||
+        (box[2].innerText != "" && box[2].innerText == box[5].innerText && box[5].innerText == box[8].innerText) ||
+        (box[0].innerText != "" && box[0].innerText == box[4].innerText && box[4].innerText == box[8].innerText) ||
+        (box[2].innerText != "" && box[2].innerText == box[4].innerText && box[4].innerText == box[6].innerText)) {
+        alert("Parabéns, você ganhou!");
+    } else if (click == 9) {
+        alert("O jogo terminou empatado!")
+    } else {
+        console.log("O jogo ainda não acabou!")
     }
 }
 
