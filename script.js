6221a24826bc523cdf6b96f90e214f0668baa005
Henrique Osiro <henriqueosiro@hotmail.com>
2019-11-13 23:00:22 -0300

    Adicionar a vitória correspondente ao jogador na função para validar

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let click = 0;
 let box = document.querySelectorAll("h1");
+let click = 0;
+let p1 = document.getElementById("p1");
+let p2 = document.getElementById("p2");
+let point1 = 0;
+let point2 = 0;
 
 function play(event) {
     if (event.target.innerHTML == "X" || event.target.innerHTML == "O") {
@@ -16,15 +20,28 @@ function play(event) {
 }
 
 function check() {
-    if ((box[0].innerText != "" && box[0].innerText == box[1].innerText && box[1].innerText == box[2].innerText) ||
-        (box[3].innerText != "" && box[3].innerText == box[4].innerText && box[4].innerText == box[5].innerText) ||
-        (box[6].innerText != "" && box[6].innerText == box[7].innerText && box[7].innerText == box[8].innerText) ||
-        (box[0].innerText != "" && box[0].innerText == box[3].innerText && box[3].innerText == box[6].innerText) ||
-        (box[1].innerText != "" && box[1].innerText == box[4].innerText && box[4].innerText == box[7].innerText) ||
-        (box[2].innerText != "" && box[2].innerText == box[5].innerText && box[5].innerText == box[8].innerText) ||
-        (box[0].innerText != "" && box[0].innerText == box[4].innerText && box[4].innerText == box[8].innerText) ||
-        (box[2].innerText != "" && box[2].innerText == box[4].innerText && box[4].innerText == box[6].innerText)) {
-        alert("Parabéns, você ganhou!");
+    if ((box[0].innerText == "X" && box[0].innerText == box[1].innerText && box[1].innerText == box[2].innerText) ||
+        (box[3].innerText == "X" && box[3].innerText == box[4].innerText && box[4].innerText == box[5].innerText) ||
+        (box[6].innerText == "X" && box[6].innerText == box[7].innerText && box[7].innerText == box[8].innerText) ||
+        (box[0].innerText == "X" && box[0].innerText == box[3].innerText && box[3].innerText == box[6].innerText) ||
+        (box[1].innerText == "X" && box[1].innerText == box[4].innerText && box[4].innerText == box[7].innerText) ||
+        (box[2].innerText == "X" && box[2].innerText == box[5].innerText && box[5].innerText == box[8].innerText) ||
+        (box[0].innerText == "X" && box[0].innerText == box[4].innerText && box[4].innerText == box[8].innerText) ||
+        (box[2].innerText == "X" && box[2].innerText == box[4].innerText && box[4].innerText == box[6].innerText)) {
+        point1++;
+        p1.innerText = point1;
+        alert("O jogador X ganhou!");
+    } else if ((box[0].innerText == "O" && box[0].innerText == box[1].innerText && box[1].innerText == box[2].innerText) ||
+        (box[3].innerText == "O" && box[3].innerText == box[4].innerText && box[4].innerText == box[5].innerText) ||
+        (box[6].innerText == "O" && box[6].innerText == box[7].innerText && box[7].innerText == box[8].innerText) ||
+        (box[0].innerText == "O" && box[0].innerText == box[3].innerText && box[3].innerText == box[6].innerText) ||
+        (box[1].innerText == "O" && box[1].innerText == box[4].innerText && box[4].innerText == box[7].innerText) ||
+        (box[2].innerText == "O" && box[2].innerText == box[5].innerText && box[5].innerText == box[8].innerText) ||
+        (box[0].innerText == "O" && box[0].innerText == box[4].innerText && box[4].innerText == box[8].innerText) ||
+        (box[2].innerText == "O" && box[2].innerText == box[4].innerText && box[4].innerText == box[6].innerText)) {
+        point2++;
+        p2.innerText = point2;
+        alert("O jogador O ganhou!");
     } else if (click == 9) {
         alert("O jogo terminou empatado!")
     } else {
@@ -33,5 +50,7 @@ function check() {
 }
 
 for (let boxs of box) {
+    p1.innerText = point1;
+    p2.innerText = point2;
     boxs.onclick = play;
 }
