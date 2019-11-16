8be104a3ce31aa0095b9e40c34f91a5328acef6b
Henrique Osiro <henriqueosiro@hotmail.com>
2019-11-16 00:00:30 -0300

    Adicionar as variáveis para salvar os nomes dos jogadores

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let box = document.querySelectorAll("h1");
 let click = 0;
+let player1 = prompt("Digite o nome do primeiro jogador:");
+let player2 = prompt("Digite o nome do segundo jogador:");
 let point1 = 0;
 let point2 = 0;
 for (let boxs of box) {
-    document.getElementById("p1").innerText = point1;
-    document.getElementById("p2").innerText = point2;
+    document.getElementById("player1").innerText = player1;
+    document.getElementById("player2").innerText = player2;
+    document.getElementById("point1").innerText = point1;
+    document.getElementById("point2").innerText = point2;
     boxs.onclick = play;
 }
 
@@ -25,16 +29,16 @@ function play(event) {
 function check() {
     if (match("X") == true) {
         point1++;
-        document.getElementById("p1").innerText = point1;
-        setTimeout(function(){ alert("O jogador venceu") }, 100);
+        document.getElementById("point1").innerText = point1;
+        setTimeout(function () { alert("O jogador venceu") }, 100);
         setTimeout(clear, 100);
     } else if (match("O") == true) {
         point2++;
-        document.getElementById("p2").innerText = point2;
-        setTimeout(function(){ alert("O jogador venceu") }, 100);
+        document.getElementById("point2").innerText = point2;
+        setTimeout(function () { alert("O jogador venceu") }, 100);
         setTimeout(clear, 100);
     } else if (click == 9) {
-        setTimeout(function(){ alert("O jogo terminou empatado!") }, 1000);
+        setTimeout(function () { alert("O jogo terminou empatado!") }, 1000);
         setTimeout(clear, 1000);
     }
 }
@@ -46,15 +50,15 @@ function clear() {
     click = 0;
 }
 
-function match (letter){
+function match(letter) {
     if ((box[0].innerText == letter && box[0].innerText == box[1].innerText && box[1].innerText == box[2].innerText) ||
-    (box[3].innerText == letter && box[3].innerText == box[4].innerText && box[4].innerText == box[5].innerText) ||
-    (box[6].innerText == letter && box[6].innerText == box[7].innerText && box[7].innerText == box[8].innerText) ||
-    (box[0].innerText == letter && box[0].innerText == box[3].innerText && box[3].innerText == box[6].innerText) ||
-    (box[1].innerText == letter && box[1].innerText == box[4].innerText && box[4].innerText == box[7].innerText) ||
-    (box[2].innerText == letter && box[2].innerText == box[5].innerText && box[5].innerText == box[8].innerText) ||
-    (box[0].innerText == letter && box[0].innerText == box[4].innerText && box[4].innerText == box[8].innerText) ||
-    (box[2].innerText == letter && box[2].innerText == box[4].innerText && box[4].innerText == box[6].innerText)){
+        (box[3].innerText == letter && box[3].innerText == box[4].innerText && box[4].innerText == box[5].innerText) ||
+        (box[6].innerText == letter && box[6].innerText == box[7].innerText && box[7].innerText == box[8].innerText) ||
+        (box[0].innerText == letter && box[0].innerText == box[3].innerText && box[3].innerText == box[6].innerText) ||
+        (box[1].innerText == letter && box[1].innerText == box[4].innerText && box[4].innerText == box[7].innerText) ||
+        (box[2].innerText == letter && box[2].innerText == box[5].innerText && box[5].innerText == box[8].innerText) ||
+        (box[0].innerText == letter && box[0].innerText == box[4].innerText && box[4].innerText == box[8].innerText) ||
+        (box[2].innerText == letter && box[2].innerText == box[4].innerText && box[4].innerText == box[6].innerText)) {
         return true;
     } else {
         return false;
